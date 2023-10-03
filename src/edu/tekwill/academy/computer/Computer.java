c2ef9da45309364bc3eddcdbd6ca26fcfb761ba0
MihaelaBoian <pinzari_m@mail.ru>
2023-10-03 15:57:31 +0300

    last

---
+++ src/edu/tekwill/academy/computer/Computer.java
@@ -0,0 +1,24 @@
+package edu.tekwill.academy.computer;
+
+import edu.tekwill.academy.computer.components.*;
+import edu.tekwill.academy.computer.peripherals.*;
+
+
+public class Computer {
+
+
+    HardDrive hardDrive;
+
+    Motherboard motherboard;
+
+    Processor processor;
+
+    Monitor monitor;
+
+    Keyboard keyboard;
+
+    Mouse mouse;
+
+
+
+}
